212c5a2d0a88fea7e0b01d19945c76f031c705ea
Arlena Müller <liadala.mueller@gmail.com>
2022-01-05 23:08:52 +0100

    möp

--- index.js
+++ index.js
@@ -3,11 +3,35 @@ const miesmuschel = require("./miesmuschel.js")
 const tmi = require('tmi.js');
 
 const client = new tmi.Client({
-	channels: [ 'liadala' ]
+	options: { 
+		debug: true,
+		joinInterval: 2000,
+		skipMembership: false,
+		skipUpdatingEmotesets: false,
+		updateEmotesetsTimer: 60000,
+	},
+	identity: {
+		username: undefined,
+		password: undefined
+	},
+	channels: [ 'liadala' ],
+	connection:{
+		reconnect:true,
+		secure: true,
+		reconnectInterval: 1000,
+		maxReconnectInverval: 30000,
+		reconnectDecay: 1.5,
+
+		maxReconnectAttempts: Infinity
+	}
 });
 
 client.connect();
 
 client.on('message', (channel, userstate, message, self) => {
-	console.log(miesmuschel.Ask(tags['display-name']));
+	var usersplit = userstate.username.split("")
+	var userA = 0
+	for (const iterator of usersplit) {
+		userA += iterator.charCodeAt(0)
+	}
 });
