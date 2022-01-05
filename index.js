11636f9703f8072d73442ee55ff8f1c156337e4f
Arlena Müller <liadala.mueller@gmail.com>
2022-01-05 23:39:21 +0100

    bla

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const miesmuschel = require("./miesmuschel.js")
 
 const tmi = require('tmi.js');
+const { channel } = require("tmi.js/lib/utils");
 
 const client = new tmi.Client({
 	options: { 
@@ -29,9 +30,40 @@ const client = new tmi.Client({
 client.connect();
 
 client.on('message', (channel, userstate, message, self) => {
-	var usersplit = userstate.username.split("")
-	var userA = 0
-	for (const iterator of usersplit) {
-		userA += iterator.charCodeAt(0)
-	}
+	if (self){ return };
+
+	(function(){
+		if (message.startsWith("!love")) {
+			var param = message.split(" ")
+			if (param.length == 0){
+				bot.say(channel, "no parameter")
+				return;
+			}
+			var usersplit = userstate.username.split("")
+			var userA = 0
+			for (const iterator of usersplit) {
+				userA += iterator.charCodeAt(0)
+			}
+
+			usersplit = param[1].split(" ")
+			var userB = 0
+			for (const iterator of usersplit) {
+				userB += iterator.charCodeAt(0)
+			}
+
+			var min = Math.min(userA,userB)
+			var max = Math.max(userA,userB)
+
+			let matching = Math.round((min/max)*100)
+
+			bot.say(channel, `@${userstate.username} and ${param[1]} love value is ${matching}%`)
+		}
+	})()
 });
+
+// ignore this its just to get output from anonymous login
+var bot = {
+	say: function(channel, message){
+		console.log(channel, message)
+	}
+}
